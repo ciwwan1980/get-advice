404d7f403f8e27dd9f9c3843f9037357443c387f
Ajil Abdalqader <ajilqado@gmail.com>
2020-06-15 01:15:49 +0200

    Updates

---
+++ precache-manifest.8e80fcbbdbac5726af4fdf534bcfb76c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ad5062bb65c8fa94b17df61a50a766",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d50a4395a2f22986276",
+    "url": "./static/css/main.7ab79fb2.chunk.css"
+  },
+  {
+    "revision": "1db741a1e5cf0b61f91a",
+    "url": "./static/js/2.73224416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.73224416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d50a4395a2f22986276",
+    "url": "./static/js/main.898cc7cf.chunk.js"
+  },
+  {
+    "revision": "69580771e13e1bdbfc0b",
+    "url": "./static/js/runtime-main.98bdf531.js"
+  },
+  {
+    "revision": "72df2fb351599644c343d3e95f92cb31",
+    "url": "./static/media/water.72df2fb3.jpg"
+  }
+]);
