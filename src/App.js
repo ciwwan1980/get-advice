5e136ff6d8b0494012eb0c4ada6106df8ad57b06
Ajil Abdalqader <ajilqado@gmail.com>
2020-06-14 23:56:38 +0200

    installing-axios-fetching

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 export default class App extends Component {
 
 state={
-
+advice:""
 }
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const advice = response.data.slip.advice;
+        console.log(advice)
+   
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render() {
 
     return (
